3d0db335b1923b439f01f96a1037982c466a02bb
Zaryab <sohail.zaryab61@gmail.com>
2020-12-27 02:29:35 -0800

    initial commit

---
+++ Ecommerce-Website/Models/Home/HomeIndexViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Ecommerce_Website.DAL;
+using Ecommerce_Website.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Ecommerce_Website.Models.Home
+{
+    
+        //public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+
+        //public IEnumerable<Tbl_Product> ListofProducts { get; set; }
+
+
+        //public static HomeIndexViewModel CreateModel()
+        //{
+        //    return new HomeIndexViewModel
+        //    {
+        //        ListofProducts = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecords() 
+        //    };
+        //}
+    public class HomeIndexViewModel
+    {
+        dbMyOnlineShoppingEntities context = new dbMyOnlineShoppingEntities();
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        public IPagedList<Tbl_Product> ListOfProducts { get; set; }
+        public HomeIndexViewModel CreateModel(string search, int pagesize ,int? page)
+            {
+
+                SqlParameter[] param = new SqlParameter[] {
+                   new SqlParameter("@search",search??(object)DBNull.Value)
+                 };
+                IPagedList<Tbl_Product> data =context.Database.SqlQuery<Tbl_Product>("GetBySearch @search",param).ToList().ToPagedList(page ?? 1, pagesize);
+          
+                return new HomeIndexViewModel
+                {
+                    ListOfProducts = data
+                };
+            }
+    }
+}
---
+++ Ecommerce-Website/PaypalConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PayPal.Api;
+
+namespace Ecommerce_Website
+{
+    public class PaypalConfiguration
+    {
+        public readonly static string clientId;
+        public readonly static string clientSecret;
+
+
+        static PaypalConfiguration()
+        {
+            var config = getconfig();
+            clientId = "AWNixhwKC5x0Rpba4u5tEXRujbcX8hkwus1tYqrH_TjAMI8kezydugLf-XRJmhw7WT4Mhgn540dB6lsv";
+            clientSecret = "EF58E3yHkH8ic7-RPdpuexdQipAFQSSP6mqTQqIGDzd93P84-xJdnaUUyjU4GObK-5-j74pQS8mWG8qa";
+        }
+
+        private static Dictionary<string, string> getconfig()
+        {
+            return PayPal.Api.ConfigManager.Instance.GetProperties();
+        }
+
+        private static string GetAccessToken()
+        {
+            string accessToken = new OAuthTokenCredential(clientId, clientSecret, getconfig()).GetAccessToken();
+            return accessToken;
+        }
+        public static APIContext GetAPIContext()
+        {
+            APIContext apicontext = new APIContext(GetAccessToken());
+            apicontext.Config = getconfig();
+            return apicontext;
+        }
+    }
+}
